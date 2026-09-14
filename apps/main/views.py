47f0b0a8f8943e6a7f45b8902c3660ebf4a0859b
Sergey Ivanov <ivanitch.mail@gmail.com>
2026-09-15 06:43:44 +0900

    chore: hello world

--- apps/main/views.py
+++ apps/main/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 def home_page(request):
     context = {
-        'title': 'ProfiFlow — сервис онлайн-записи для бьюти-мастеров и студий.',
-        'h1': 'ProfiFlow',
+        'title': 'ProfiFlow 🦋 сервис онлайн-записи для бьюти-мастеров и студий',
+        'h1': 'Hello, world! 👋',
     }
     return render(request, 'main/home.html', context)
