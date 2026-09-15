1bd670f99c278ee0391b76ebd304bdb281664ba3
Sergey Ivanov <ivanitch.mail@gmail.com>
2026-09-15 23:29:41 +0900

    feat: client page + form appointment in a modal window

--- apps/main/urls.py
+++ apps/main/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 from .views import (
+    source_page,
     home_page,
+    client_page,
 )
 
 app_name = 'main'
 
 urlpatterns = [
     path('', home_page, name='home'),
+    path('source-page/', source_page, name='source-page'),
+    path('client-page/', client_page, name='client-page'),
 ]
--- apps/main/views.py
+++ apps/main/views.py
@@ -6,5 +6,11 @@ def home_page(request):
         'title': 'ProfiFlow 🦋 сервис онлайн-записи для бьюти-мастеров и студий',
         'h1': 'Hello, world! 👋',
     }
-    
+
     return render(request, 'main/home.html', context)
+
+def source_page(request):
+    return render(request, 'main/source.html')
+
+def client_page(request):
+    return render(request, 'main/client.html')
